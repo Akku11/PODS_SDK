7bc6e70ab46a0a56bd77af1ff4930295db75c0df
Akanksha <>
2024-06-20 02:47:02 +0800

    Added static framework to app

---
+++ TeamsApp/TeamsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamsApp
+//
+//  Created by Akanksha Thakur on 20/6/24.
+//
+
+import UIKit
+import MyTeamsFramework
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    Teams().getName()
+  }
+
+
+}
+
